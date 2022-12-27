44188732c44091788a36dac56442163acd07adeb
Yulia Korotkova <y.korotkova@just-ai.com>
2022-12-27 18:22:01 +0000

    update inference

--- inference.py
+++ inference.py
@@ -3,7 +3,8 @@ import torch
 from nltk.tokenize import word_tokenize
 import argparse
 from pathlib import Path
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
+from torch import nn
 
 MODEL_NAME = 'sberbank-ai/ruRoberta-large'
 
@@ -23,19 +24,59 @@ int_to_cat = [
 int_to_sent = ["positive", "negative", "neutral", "both"]
 labels = ["absence", "positive", "negative", "both", "neutral"]
 
+class CatClassifier(nn.Module):
+
+    def __init__(self, n_classes):
+        super(CatClassifier, self).__init__()
+        self.bert = AutoModel.from_pretrained(MODEL_NAME)
+        self.drop = nn.Dropout(p=0.5)
+        self.Food = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.Interior = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.Price = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.Whole = nn.Linear(self.bert.config.hidden_size, n_classes)
+        self.Service = nn.Linear(self.bert.config.hidden_size, n_classes)
+  
+    def forward(self, input_ids, attention_mask):
+        outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask
+        )
+        output = self.drop(self.mean_pooling(outputs, attention_mask))
+        Food = self.Food(output)
+        Interior = self.Interior(output)
+        Price = self.Price(output)
+        Whole = self.Whole(output)
+        Service = self.Service(output)
+        return {
+            "Food": Food, 
+            "Interior": Interior, 
+            "Price": Price, 
+            "Whole": Whole, 
+            "Service": Service, 
+            }
+    
+    @staticmethod
+    def mean_pooling(model_output, attention_mask):
+        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+        sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+        sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+        return sum_embeddings / sum_mask
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Inference on Aspect-Sentiment tasks")
     parser.add_argument("--first_task", action="store_true")
     parser.add_argument("--no-first_task", dest="first_task", action="store_false")
-    parser.set_defaults(first_task=True)
+    parser.set_defaults(first_task=False)
     parser.add_argument("--second_task", action="store_true")
     parser.add_argument("--no-second_task", dest="second_task", action="store_false")
-    parser.set_defaults(second_task=True)
+    parser.set_defaults(second_task=False)
     parser.add_argument("--aspect_filename", type=Path, required=False, help="Path to file where to write aspects")
     parser.add_argument("--sentiment_filename", type=Path, required=False, help="Path to file where to write sentiments")
     parser.add_argument("--reviews_filename", type=Path, required=True, help="Path to file with reviews")
-    parser.add_argument("--device1", type=str, required=False, help="device", default="cpu")
-    parser.add_argument("--device2", type=str, required=False, help="device", default="cpu")
+    parser.add_argument("--device1", type=str, required=False, help="device for model_cat", default="cpu")
+    parser.add_argument("--device2", type=str, required=False, help="device for model_sent", default="cpu")
+    parser.add_argument("--device3", type=str, required=False, help="device for model", default="cpu")
     parser.add_argument("--model_cat", type=Path, required=False, help="Path to the model for sequence labeling by category")
     parser.add_argument("--model_sent", type=Path, required=False, help="Path to the model for sequence labeling by sentiment")
     parser.add_argument("--model", type=Path, required=False, help="Path to the model for sentiment classification")
@@ -107,11 +148,11 @@ def inference_aspects(review, model_cat, model_sent, tokenizer, idx, device_1, d
             length += e
     return output
 
-def inference_sentiment(tokenizer, model, text):
+def inference_sentiment(tokenizer, model, text, device):
     encoding = tokenizer(text, return_tensors='pt')
     model = model.eval()
     with torch.no_grad():
-        pred = model(encoding)
+        pred = model(input_ids=encoding["input_ids"].to(device), attention_mask=encoding["attention_mask"].to(device))
     _, Food = torch.max(pred["Food"], dim=1)
     _, Interior = torch.max(pred["Interior"], dim=1)
     _, Price = torch.max(pred["Price"], dim=1)
@@ -148,7 +189,7 @@ def main(args):
 
     if args.second_task and args.model:
         model = torch.load(args.model)
-        _ = model.to(args.device1)
+        _ = model.to(args.device3)
 
         if args.sentiment_filename and args.reviews_filename:
             with open(args.sentiment_filename, "w", encoding="utf-8") as file_write:
@@ -156,7 +197,7 @@ def main(args):
                     for line in file_read:
                         line = line[:-1]
                         idx, text = line.split("\t")
-                        pred = inference_sentiment(tokenizer, model, text)
+                        pred = inference_sentiment(tokenizer, model, text, args.device3)
                         for cat in pred:
                             file_write.write(f"{idx}\t{cat}\t{pred[cat]}\n")
         else:
